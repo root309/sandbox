a59a01f045984cdd2b517293d94e15925853057a
root309 <choco00101x@gmail.com>
2024-02-18 19:16:57 +0900

    fizzbuzz

--- fizzbuzz.zig
+++ fizzbuzz.zig
@@ -3,12 +3,15 @@ const print = @import("std").debug.print;
 const info = @import("std").debug.log.info;
 
 pub fn main() void {
-    for (0..101) |i| {
-        if (i % 3 == 0) {
-            print("Fizz {}\n", .{i});
+    for (1..101) |i| {
+        if (i % 3 == 0 and i % 5 == 0) {
+            print("FizzBuzz\n", .{});
+        } else if (i % 3 == 0) {
+            print("Fizz\n", .{});
         } else if (i % 5 == 0) {
-            print("Buzz {}\n", .{i});
+            print("Buzz\n", .{});
+        } else {
+            print("{}\n", .{i});
         }
-        print("For: Iteration {}\n", .{i});
     }
 }
